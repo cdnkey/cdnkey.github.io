6be9d1638d8276a8ef6a5fd33a84b20a1de621e8
cdnkey <cdnkey@protonmail.com>
2024-06-16 16:24:05 +0300

    Update dmca.js

--- dmca.js
+++ dmca.js
@@ -2,7 +2,7 @@ videoPlayer.addEventListener('canplaythrough', (event) => {
 let DmcaBanner = document.querySelector('.dmca-banner');
 	if (DmcaBanner) {
                 DmcaBanner.remove();
+		videoPlayer.style.filter = 'none';
+		videoPlayer.style.transition = '300ms all ease';
             }
-            videoPlayer.style.filter = 'none';
-            videoPlayer.style.transition = '300ms all ease';
         });
