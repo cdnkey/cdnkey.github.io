5d669665ad23b74ffe47d271387cbbdf68c651ac
cdnkey <cdnkey@protonmail.com>
2024-06-27 16:22:48 +0300

    Update dmca.js

--- dmca.js
+++ dmca.js
@@ -2,9 +2,9 @@ function onVideoCanPlayThrough() {
             const dmcaBanner = document.querySelector('.dmca-banner');
             if (dmcaBanner) {
                 dmcaBanner.remove();
+                document.body.pointerEvents = 'auto';
             }
             videoPlayer.style.filter = 'none';
-            document.body.pointerEvents = 'auto';
         }
 
         videoPlayer.addEventListener('canplaythrough', onVideoCanPlayThrough);
