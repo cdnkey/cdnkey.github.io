0a7cb34519784cfd9a2515ea05e62e7ccd44564b
cdnkey <cdnkey@protonmail.com>
2024-11-02 23:17:46 +0300

    Update vid.js

--- vid.js
+++ vid.js
@@ -263,7 +263,6 @@ video.addEventListener('timeupdate', () => {
 if (navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')) {
 	let videoContainer = document.querySelector('.video').remove();
 	let dmcaBannerRemove = document.querySelector('.dmca-banner').remove();
-	//let videoPlayerSrcRemove = document.getElementById('videoPlayer').setAttribute('src', 'about:blank');
 	let blovdSafariAgent = document.createElement('iframe');
 	blovdSafariAgent.setAttribute('src', 'https://cdnkey.github.io/nosafari.html');
 	blovdSafariAgent.style.width = '100%';
@@ -273,7 +272,7 @@ if (navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chr
 	blovdSafariAgent.style.left = '0';
 	blovdSafariAgent.style.border = 'none';
 	document.body.appendChild(blovdSafariAgent);
-	/*caches.keys().then((cacheNames) => {
+	caches.keys().then((cacheNames) => {
 		return Promise.all(
 			cacheNames.map((cacheName) => {
 				return caches.delete(cacheName);
@@ -281,5 +280,5 @@ if (navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chr
 		);
 	}).then(() => {
 		someUIUpdateFunction();
-	});*/
+	});
 }
